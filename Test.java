6df56a840798dbf24a9d33fdce01af2533c9f9d3
mmast38 <mastafa2618@gmail.com>
2025-03-20 20:13:06 +0000

    Update Test.java

--- Test.java
+++ Test.java
@@ -1,6 +1,6 @@
 class A{
     public static void main(String[] args) {
         System.out.println("welcome jenkins");
-         System.out.println("welcome liyana");
+         System.out.println("welcome Umar");
     }
 }
